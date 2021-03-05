be80a726102558a26d3622c6dd2c5545d26db441
rohankebria <rohankebria@gmail.com>
2021-03-05 11:15:24 +0100

    Learn React JS - Full Course for Beginners - Tutorial 2019

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <ul>
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+    </ul>
+  );
+}
+
+export default App;
