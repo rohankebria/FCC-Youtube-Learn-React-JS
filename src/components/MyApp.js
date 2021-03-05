642713c5d7e21bd353c58f1f3883af475e592975
rohankebria <rohankebria@gmail.com>
2021-03-05 13:05:57 +0100

    Learn React FreeCodeCamp- Youtube - Styling Object modification

--- src/components/MyApp.js
+++ src/components/MyApp.js
@@ -3,21 +3,19 @@ import React from "react";
 function MyApp() {
   const firstName = "Roohan";
   const date = new Date();
+  const styleMy = { backgroundColor: "green", color: "white", fontSize: 55 };
+
   return (
     <div>
-      <h1> Hello World and {firstName}</h1>
+      <h1 style={styleMy}> Hello World and {firstName}</h1>
       <h1> Hello Sweden and {`${firstName}`}</h1>
       <h3 style={{ backgroundColor: "red", color: "white" }}>
         its currently about {date.getHours() < 13 ? "Noon" : "Morning"} Time in
         Stocholm
       </h3>
-      <input
-        style={{ backgroundColor: "red" }}
-        type="checkbox"
-        id="vehicle1"
-        name="vehicle1"
-        value="Bike"
-      ></input>
+      {((styleMy.color = "yellow"), console.log(styleMy))}
+      <h4 style={{ styleMy }}>acess and change style object</h4>
+      <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"></input>
       <label for="vehicle1"> I have a bike</label>
       <br></br>
       <input type="checkbox" id="vehicle2" name="vehicle2" value="car"></input>
